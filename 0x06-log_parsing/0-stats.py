fad8fb40884fba9fa25cd652dc32d200d925b4cc
jdcera4 <jdcera4@gmail.com>
2022-02-04 08:46:05 -0500

    update task 0

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -12,75 +12,50 @@ Fields:
 """
 
 
-def print_log_totals(total_file_size, code_counts):
-    """
-    Prints current totals of file size and status code counts.
-    Args:
-        total_file_size (int): cumulative total of bytes received through
-            GET requests in log
-        code_counts (OrderedDict): totals of status codes from repsonses
-    """
-    print("File size: {}".format(total_file_size))
-    for code in code_counts:
-        if code_counts[code] > 0:
-            print("{}: {}".format(code, code_counts[code]))
+import sys
 
+if __name__ == "__main__":
 
-if __name__ == '__main__':
-    from sys import argv, stdin, stderr
-    from collections import OrderedDict
-    from datetime import datetime
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
+                    403: 0, 404: 0, 405: 0, 500: 0}
+    file_size = [0]
+    count = 1
 
-    line_no = 0
-    total_file_size = 0
-    code_counts = OrderedDict.fromkeys([200, 301, 400, 401, 403,
-                                        404, 405, 500], 0)
+    def print_stats():
+        '''
+        Prints file size and stats for every 10 loops
+        '''
+        print('File size: {}'.format(file_size[0]))
+
+        for code in sorted(status_codes.keys()):
+            if status_codes[code] != 0:
+                print('{}: {}'.format(code, status_codes[code]))
+
+    def parse_stdin(line):
+        '''
+        Checks the stdin for matches
+        '''
+        try:
+            line = line[:-1]
+            word = line.split(' ')
+            # File size is last parameter on stdout
+            file_size[0] += int(word[-1])
+            # Status code comes before file size
+            status_code = int(word[-2])
+            # Move through dictionary of status codes
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+        except BaseException:
+            pass
 
     try:
-        for line in stdin:
-            line_no += 1
-
-            # both ip addresses/URLs allowed, but some lines may be raw text
-            a = line.split('-', 1)
-            if len(a) != 2:
-                # likely not a formatted line
-                continue
-
-            # checking timestamp
-            b = a[1].split(']')
-            timecode = b[0].lstrip(' [')
-            try:
-                datetime.strptime(timecode, '%Y-%m-%d %H:%M:%S.%f')
-            except:
-                stderr.write("{}: {}: invalid timecode\n".format(
-                    argv[0], line_no))
-                pass
-
-            # checking URL
-            c = b[1].split('"')
-            c = c[1:]
-            if c[0] != 'GET /projects/260 HTTP/1.1':
-                stderr.write("{}: {}: unexpected HTTP request\n".format(
-                    argv[0], line_no))
-
-            # prep for status code and file size
-            d = c[1].lstrip(' ')
-            d = d.rstrip('\n')
-            d = d.split(' ')
-
-            # checking status code (invalid codes skipped without error)
-            if d[0].isdecimal():
-                code = int(d[0])
-                code_counts[code] += 1
-
-            # checking file size (invalid sizes skipped without error)
-            if d[1].isdecimal():
-                total_file_size += int(d[1])
-
-            if line_no % 10 == 0:
-                print_log_totals(total_file_size, code_counts)
-        print_log_totals(total_file_size, code_counts)
-
-    except (KeyboardInterrupt):
-        print_log_totals(total_file_size, code_counts)
+        for line in sys.stdin:
+            parse_stdin(line)
+            # print the stats after every 10 outputs
+            if count % 10 == 0:
+                print_stats()
+            count += 1
+    except KeyboardInterrupt:
+        print_stats()
         raise
+    print_stats()
